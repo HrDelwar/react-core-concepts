4500128950fef75a839fa8c3eafcf5f87f5e20ec
HrDelwar <hrdelwar75@gmail.com>
2021-02-23 01:00:15 +0600

    component simlar look different data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
-const capitalize = {
-  textTransform:'capitalize',
+const container = {
+  border:"2px solid rgba(0, 0, 0, .5)",
+  borderRadius:'5px',
+  width:"50%",
+  margin:"15px auto"
 }
+
 const button ={
   backgroundColor:'rgba(0,0,0,.5)',
   padding:'10px 30px',
@@ -17,41 +20,55 @@ function App() {
   const orange = {
     color: 'orange'
   }
- 
+  const heading = {
+    textTransform:'capitalize',
+    margin:"5rem 0 2rem 0",
+    color:"rgba(0,0,0,.7)"
+  }
   const person = {
     name:'Hero alom',
-    type: 'funny'
+    type: 'funny person'
   }
   return (
     <div className="App">
       <header className="App-header">
+        <h1 style={orange}>React JSX</h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 style={{ color: 'cyan' }, capitalize}>Welcome to react </h1>
-        <h2 style={orange,capitalize}>react JSX</h2>
-        <h3>{person.name} is a {person.type} person.</h3>
+        <h2 style={{ color: 'cyan' }}>Welcome To React </h2>
+        <h3>{person.name} is a {person.type} .</h3>
         <button style={button}>Simple button</button>
       </header>
-      <h1 style={capitalize}>Component similar look and similar data</h1>
+      <h1 style={heading}>Component! Similar look and similar data</h1>
       <Singer></Singer>
       <Singer></Singer>
       <Singer></Singer>
       <Singer></Singer>
+      <h1 style={heading}>Component! Similar look and different data</h1>
+      <Person name={person.name} type={person.type +"🤣"} qty="Best funny" btn="Hero ^_^" ></Person>
+      <Person name="Dr.Mahfuzur Rahman" type="Singer 🐸" qty="Best " btn="Dr.Litu ಠ_ಠ" ></Person>
+      <Person name="Eva Rahman" type="Singer🙄" qty="Kokil konthi" btn="Kokil konthi (•_•)"></Person>
     </div>
   );
 }
 
 
 function Singer(){
-  const style = {
-    border:"2px solid rgba(0, 0, 0, .5)",
-    margin:'10px',
-    borderRadius:'5px'
-  }
+ 
   return(
-    <div className="" style={style} >
+    <div className="" style={container} >
       <h2>Dr.Mahfuzur Rahman</h2>
       <p>Best singer of the year</p>
       <button style={button}>Details</button>
+    </div>
+  );
+}
+
+function Person(props){
+  return(
+    <div className="" style={container}>
+        <h2>{props.name} is a {props.type}</h2>
+        <p>{props.qty} singer of the year...🤣</p>
+        <button style={button }>More About {props.btn}</button>
     </div>
   );
 }
